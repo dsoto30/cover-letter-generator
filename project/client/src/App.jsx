c071e0272b3627e9f195e0364ecabf518ae31d6b
dsoto30 <sotodaniel531@gmail.com>
2024-03-15 19:34:22 -0700

    Added Navigate component to redirect to auth/login page when reaching root of react app

--- project/client/src/App.jsx
+++ project/client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Auth } from "./pages/Auth";
 import { Error } from "./pages/Error";
 import { Home } from "./pages/Home";
@@ -8,6 +8,7 @@ export default function App() {
     return (
         <>
             <Routes>
+                <Route exact path="/" element={<Navigate to="auth/login" />}> </Route>
                 <Route path="/auth/*" element={<Auth />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="*" element={<Error />} />
